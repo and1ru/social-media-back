7671b23598b836bad17bb8712c9211849e4e0f8d
and1ru <andres.vanegas2700@gmail.com>
2026-06-09 17:53:31 -0500

    se puso el login en una ruta

--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
 import express from 'express'
 import { Router } from 'express'
 import { RegisterController } from './src/register/register.controller.ts'
-
+import { LoginController } from "./src/login/login.controller.ts"
 const app = express()
 app.use(express.json())
 const routes = Router()
 const register = new RegisterController()
+const login = new LoginController()
 
 routes.get("/", (req, res) => {
     res.send("todo funciona")
 })
 
 routes.post("/register", register.register)
+routes.post("/login", login.login)
 
 app.use(routes)
 
