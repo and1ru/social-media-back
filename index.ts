bb8591b8c75e3a185d5eac956e01612639e0a1b8
and1ru <andres.vanegas2700@gmail.com>
2026-06-09 20:57:13 -0500

    se actualiza el index.ts

--- index.ts
+++ index.ts
@@ -2,11 +2,15 @@ import express from 'express'
 import { Router } from 'express'
 import { RegisterController } from './src/register/register.controller.ts'
 import { LoginController } from "./src/login/login.controller.ts"
+import { CreatePostController } from './src/create_post/post.controller.ts'
+import { GetPostsController } from './src/get_posts/get_posts.controller.ts'
 const app = express()
 app.use(express.json())
 const routes = Router()
 const register = new RegisterController()
 const login = new LoginController()
+const createPost = new CreatePostController()
+const get_posts = new GetPostsController()
 
 routes.get("/", (req, res) => {
     res.send("todo funciona")
@@ -14,6 +18,8 @@ routes.get("/", (req, res) => {
 
 routes.post("/register", register.register)
 routes.post("/login", login.login)
+routes.post("/create-post", createPost.post)
+routes.get("/get-posts", get_posts.getPost)
 
 app.use(routes)
 
