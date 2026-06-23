f43c216ec7594971189cdb67044409c09819ded4
and1ru <andres.vanegas2700@gmail.com>
2026-06-23 14:34:57 -0500

    se completo el middleware de errores

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import { routes } from './src/app.routes.ts'
+import { errorMiddleware } from './src/middlewares/error.middleware.ts'
 
 const app = express()
 
 app.use(cors({ origin: "*" }))
 app.use(express.json())
 app.use(routes) 
+
+// middleware de errores (SIEMPRE AL FINAL)
+app.use(errorMiddleware);
 
 app.listen(3000, () => {
     console.log("Servidor escuchando en el puerto 3000")
--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,20 +1,28 @@
-export class CustomError extends Error {
-    public status:number
-
-    constructor(message:string, status:number){
-        super(message)
-        this.status = status
-    }
-}
-
 import type { Request, Response, NextFunction } from "express"
+import jwt from 'jsonwebtoken'
+import { CustomError } from "../helpers/custom-error"
+import { ZodError } from "zod"
 
-export const errorMiddleware = (err:Error | CustomError, req:Request, res:Response, next:NextFunction) => {
+export const errorMiddleware = (err: Error | CustomError, req: Request, res: Response, next: NextFunction) => {
+    // otros errores
+    if (err instanceof CustomError) {
+        return res.status(err.status).json({ success: false, message: err.message })
+    }
 
     // ver si es un error de jsonwebtoken
-    // si es un error de tipado de zod
-    // otros errores
-    // error de servidor
+    if (err instanceof jwt.JsonWebTokenError) {
+        return res.status(401).json({ success: false, message: "token no valido" })
+    }
 
-    return res.status(500).json({message:"error del servidor"})
+    if (err instanceof jwt.TokenExpiredError) {
+        return res.status(401).json({ success: false, message: "El token de sesión ha expirado" })
+    }
+
+    // si es un error de tipado de zod
+    if (err instanceof ZodError) {
+        return res.status(400).json({ success: false, message: "error en los campos" })
+    }
+
+    // error de servidor
+    return res.status(500).json({ success: false, message: "error del servidor" })
 }
