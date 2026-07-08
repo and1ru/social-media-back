3a3bf83c3b7069ff2ca6f47f91d6488915bb0f5c
and1ru <andres.vanegas2700@gmail.com>
2026-07-07 20:58:31 -0500

    se configuro todo para que funciones los sockets

--- src/send_message/send_message.schema.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod"
-
-export const sendMessageSchema = z.object({
-    date: z.date(),
-    text: z.string(),
-    userId: z.string()
-})
-
-export type sendMessageType = z.infer<typeof sendMessageSchema>
--- src/socket.routes.ts
+++ src/socket.routes.ts
@@ -1,5 +1,5 @@
 import { Server } from "socket.io";
-import { chatSocket } from "./send_message/send_message.controller.ts";
+import { chatSocket } from "./send_message/send_message.socket.ts";
 
 // crea una funcion que reciba todos los sockets
 // para que en el index pueda importar solo esta funcion y se ejecuten el resto
